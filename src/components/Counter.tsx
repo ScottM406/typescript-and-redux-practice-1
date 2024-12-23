afcd1e9a4aa377fc6f4f170e57707982dd3a23b8
Scott <Scottyjmt13@gmail.com>
2024-12-22 23:11:42 -0700

    Add counter actions

---
+++ src/components/Counter.tsx
@@ -0,0 +1,24 @@
+import { decrement, increment, incrementByAmount, incrementAsync } from "../state/counter/counterSlice";
+import { AppDispatch, RootState } from "../state/store";
+import { useDispatch, useSelector } from "react-redux";
+
+const Counter = () => {
+
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => dispatch(increment())}>Inrease</button>
+        <button onClick={() => dispatch(decrement())}>Decrease</button>
+        <button onClick={() => dispatch(incrementByAmount(1000))}>Increase by 1000</button>
+        <button onClick={() => dispatch(incrementAsync(1000))}>Increase by 1000 asynchronously</button>
+      </div>
+    </>
+    
+  )
+}
+
+export default Counter;
